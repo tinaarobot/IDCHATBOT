1052ebd04a4d97dcb14f392f26dbdb11841dee7b
venom bolteop <145913044+venombolteop@users.noreply.github.com>
2024-01-25 18:14:46 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 API_ID = int(getenv("API_ID", 27353035))
 API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
-BOT_TOKEN = getenv("BOT_TOKEN", None)
+STRING_SESSION = getenv("STRING_SESSION", None)
 OWNER_ID = int(getenv("OWNER_ID", "6306738739"))
 MONGO_URL = getenv("MONGO_URL", None)
 SUPPORT_GRP = getenv("SUPPORT_GRP", "Venom_Chatz")
