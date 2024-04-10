b6439f86359f486ea5b63585541e693eae90f5d9
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-10 14:59:20 +0530

    Update stats.py

--- ROYEDITX/modules/stats.py
+++ ROYEDITX/modules/stats.py
@@ -1,19 +1,19 @@
 from pyrogram import filters, Client
 from pyrogram.types import Message
 
-from Venom import OWNER, VenomX
-from Venom.database.chats import get_served_chats
-from Venom.database.users import get_served_users
+from ROYEDITX import OWNER, ROYX
+from ROYEDITX.database.chats import get_served_chats
+from ROYEDITX.database.users import get_served_users
 
 
-@VenomX.on_message(filters.command("stats") & filters.user(OWNER))
+@ROYX.on_message(filters.command("stats") & filters.user(OWNER))
 async def stats(cli: Client, message: Message):
     users = len(await get_served_users())
     chats = len(await get_served_chats())
     await message.reply_text(
-        f"""ᴛᴏᴛᴀʟ sᴛᴀᴛs ᴏғ {(await cli.get_me()).mention} :
+        f"""✦ ᴛᴏᴛᴀʟ sᴛᴀᴛs ᴏғ {(await cli.get_me()).mention} :
 
-➻ **ᴄʜᴀᴛs :** {chats}
-➻ **ᴜsᴇʀs :** {users}"""
+✦ **ᴄʜᴀᴛs ➠** {chats}
+✦ **ᴜsᴇʀs ➠** {users}"""
     )
   
