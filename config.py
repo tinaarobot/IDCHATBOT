0ed47b9e0d332bcfc75528398f383d3c929ba1ea
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-28 17:50:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,29 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
+### ❖ ➥
 API_ID = int(getenv("API_ID", None))
+
+### ❖ ➥
 API_HASH = getenv("API_HASH", None)
+
+### ❖ ➥
 STRING_SESSION = getenv("STRING_SESSION", None)
+
+### ❖ ➥
 OWNER_ID = int(getenv("OWNER_ID", "6922271843"))
+
+### ❖ ➥
 MONGO_URL = getenv("MONGO_URL", None)
+
+### ❖ ➥
 SUPPORT_GRP = getenv("SUPPORT_GRP", "THE_FRIENDZ")
+
+### ❖ ➥
 UPDATE_CHNL = getenv("UPDATE_CHNL", "ROY_EDITX")
+
+### ❖ ➥
 OWNER_USERNAME = getenv("OWNER_USERNAME", "LOCO_X_PILOT")
 
 
