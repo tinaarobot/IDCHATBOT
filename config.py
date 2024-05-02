cd5ef73e7653ab21e30bf13de1284b644286252d
Redeyealone <154379249+Redeyealone@users.noreply.github.com>
2024-05-02 14:06:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,18 +14,18 @@ API_HASH = getenv("API_HASH", None)
 STRING_SESSION = getenv("STRING_SESSION", None)
 
 ### ❖ ➥
-OWNER_ID = int(getenv("OWNER_ID", "6922271843"))
+OWNER_ID = int(getenv("OWNER_ID", "5483829443"))
 
 ### ❖ ➥
 MONGO_URL = getenv("MONGO_URL", None)
 
 ### ❖ ➥
-SUPPORT_GRP = getenv("SUPPORT_GRP", "THE_FRIENDZ")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "Dark_Support_Group)
 
 ### ❖ ➥
-UPDATE_CHNL = getenv("UPDATE_CHNL", "ROY_EDITX")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "TheDark_Support")
 
 ### ❖ ➥
-OWNER_USERNAME = getenv("OWNER_USERNAME", "LOCO_X_PILOT")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "redeye_alone)
 
 
